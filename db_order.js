9aab353d0611ea4b7b5993837697fac270aacb7f
NGUYEN QUOC DAI <zane.nguyen@unicorn-fintech.com>
2019-03-27 09:35:31 +0700

    fix

--- db_order.js
+++ db_order.js
@@ -1,29 +1,32 @@
-var data ={
-    "id": 1,
-    "user": {
-        "firstName": '',
-        "lastName": '',
-        "nickName": '',
-        "email": '',
-        "address1": '',
-        "address2": '',
-        "country": '',
-        "state": '',
-        "zip": '',
-        "phone": ''
-            },
-    "cart": {
-        "products":
-            {
-                "id": '',
-                "quantity": '',
-                "amount": ''
-            },
-        "totalQuantity": '',
-        "totalAmount": '',
-          },
-    "date": '',
-    "status": ''
-}
+var data = [
+    {
+        "id": 1,
+        "user": {
+            "firstName": '',
+            "lastName": '',
+            "nickName": '',
+            "email": '',
+            "address1": '',
+            "address2": '',
+            "country": '',
+            "state": '',
+            "zip": '',
+            "phone": ''
+        },
+        "cart": {
+            "products": [
+                {
+                    "id": '',
+                    "quantity": '',
+                    "amount": ''
+                }
+            ],
+            "totalQuantity": '',
+            "totalAmount": '',
+        },
+        "date": '',
+        "status": ''
+    }
+]
 
 module.exports = data;

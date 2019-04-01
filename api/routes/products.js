b852e208c375df5a045b753b6dc6b4a02a93e7a2
quocdai97 <zane.nguyen@unicorn-fintech.com>
2019-04-02 00:35:47 +0700

    add mongodb

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const controller = require('../controller/products');
+
 
 router.get('/api/products', (req, res) => {
     res.send(controller.getProducts(req.query));
--- app.js
+++ app.js
@@ -4,6 +4,27 @@ const bodyParser = require('body-parser');
 // const port = 4000;
 const port = process.env.PORT;
 
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+
+app.use('/', function(req, res, next){
+    var resultArray = [];
+    MongoClient.connect(url, (err, db) => {
+        var dbo = db.db("local");
+        if (err) throw err;
+       var cursor = dbo.collection("demo").find({}).toArray(function(err, result){
+            if (err) throw err;
+           // console.log(result);
+            
+            res.send(result);
+        },  function(){
+            db.close();
+           // res.render(result)
+        })
+    })
+})
+
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
